5f0202a74b6132127be8b8e23bebb1145511ac17
Angela Zhao <angela.zhao2006@gmail.com>
2025-07-08 16:33:09 -0400

    Redesigned website
    Added a popup welcoming visitors :3

--- scipt.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const passport = document.getElementById("passport");
-  passport.addEventListener("click", function() {
-    passport.classList.toggle("open");
-  });
-});
